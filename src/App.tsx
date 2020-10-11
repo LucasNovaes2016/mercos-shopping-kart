5d0819d5bb73de88aa621b34083637285b1b3f54
Lucas Novaes <lucasmsn200@gmail.com>
2020-10-11 12:26:39 -0300

    feat(geral): utilizando bootstrap e font awesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1> Hello World </h1>
-    </div>
+    <>
+      <Router>
+        <div className="container">
+          <h1> Hello World from container </h1>
+          <ToastContainer
+            autoClose={5000}
+            position={toast.POSITION.BOTTOM_RIGHT}
+          />
+        </div>
+      </Router>
+    </>
   );
 }
 
